bff3722579d4181c68ecd0707ffdd8c35765fefe
nicolalockwood <nicolahelenlockwood@gamil.com>
2022-03-28 16:58:52 +0100

    #12 article_id endpoint completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -32,3 +32,30 @@ describe('GET /api/topics', () => {
 			});
 	});
 });
+
+describe('GET /api/articles/:article_id', () => {
+	test('200: responds with an articles object, with the username as from users as author', () => {
+		return request(app)
+			.get('/api/articles/1')
+			.expect(200)
+			.then((res) => {
+				expect(res.body.article).toEqual({
+					article_id: 1,
+					title: 'Living in the shadow of a great man',
+					topic: 'mitch',
+					author: 'butter_bridge',
+					body: 'I find this existence challenging',
+					created_at: expect.any(String),
+					votes: 100,
+				});
+			});
+	});
+	test('404: Responds with message for valid but none recognised article ID', () => {
+		return request(app)
+			.get('/api/articles/1000')
+			.expect(404)
+			.then((res) => {
+				expect(res.body).toMatchObject({ msg: 'Article not found' });
+			});
+	});
+});
--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,9 +1,18 @@
-const { selectTopics } = require('../models/news.model');
+const { selectTopics, selectArticleID } = require('../models/news.model');
 
 exports.getTopics = (req, res, next) => {
 	selectTopics()
 		.then((topics) => {
 			res.status(200).send({ topics });
+		})
+		.catch((err) => next(err));
+};
+
+exports.getArticleID = (req, res, next) => {
+	const { article_id } = req.params;
+	selectArticleID(article_id)
+		.then((article) => {
+			res.status(200).send({ article });
 		})
 		.catch((err) => next(err));
 };
